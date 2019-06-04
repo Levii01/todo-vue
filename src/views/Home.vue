b6ffbbaa0d37825357185c60cc8db6f1937dbc24
Levii01 <m.sedziak01@gmail.com>
2019-06-04 17:04:30 +0200

    Example todo view in home app

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="@/assets/logo.png" id="vue-logo">
+    <div class="title">What do I need to do today?</div>
+    <input v-model="myTodo" /><button @click="addToDo">Add</button>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-@Component({
-  components: {
-    HelloWorld,
+<script>
+export default {
+  name: 'home',
+  data: function () {
+    return {
+      myTodo: ''
+    }
   },
-})
-export default class Home extends Vue {}
+  methods: {
+    addToDo: function () {
+      console.log('myTodo: ' + this.myTodo)
+    }
+  }
+}
 </script>
+
+<style>
+* {
+  box-sizing:border-box;
+}
+
+body, html, #app {
+  background:#8ac8e5;
+}
+
+.home {
+  width:300px;
+  margin:auto;
+}
+
+#vue-logo {
+  width:100px;
+}
+
+input, button {
+  border:0;
+  width:100%;
+  margin:0 0 10px;
+  padding:7px;
+}
+
+input {
+  font-size:12px;
+}
+
+button {
+  background:#43b823;
+  border:0;
+  text-transform:uppercase;
+  color:#fff;
+  font-weight:700;
+  cursor:pointer;
+}
+
+.title {
+  font-size:14px;
+  font-weight:700;
+  padding:0 0 10px 0;
+  margin:0 0 10px 0;
+  border-bottom:1px solid #666;
+}
+
+#errors {
+  background:#a52222;
+  color:#fff;
+  padding:5px;
+}
+</style>
